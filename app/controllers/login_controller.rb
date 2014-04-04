a7bea07706c2949f80c9ce389640d2d4fb7c6b61
dinesh <dineshswamy92@gmail.com>
2014-04-04 16:00:34 +0530

    with empty commits

--- app/controllers/login_controller.rb
+++ app/controllers/login_controller.rb
@@ -2,9 +2,7 @@ class LoginController < ApplicationController
 	
 	skip_before_filter :verify_authenticity_token
 	before_filter :protect_from_forgery,:except => [:register]
-#psql "dbname=d86m5ohsqb9c5a host=ec2-23-23-177-33.compute-1.amazonaws.com user=ddylynfbeycyam password=pfDxgJggA3HmcepFr1aeuPkIK7 port=5432 sslmode=require"
 	def register()
-
 		begin
 			 if(!params[:email_id].nil? && !params[:channel_id].nil? && !params[:name].nil?)
 			 	new_user= User.new
